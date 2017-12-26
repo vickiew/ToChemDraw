a119b3c1d8c6f1a88ad19ef9552101743dac3ccd
BuchwaldGroup <buchwaldgroup@MavericksImage-2.local>
2017-12-25 20:49:44 -0500

    fix typos

--- Bond.py
+++ Bond.py
@@ -10,14 +10,14 @@ class Bond(object):
         self.stereo = stereo
 
     def __str__(self):
-        """Returns the cdxml string representation of this Node"""
+        """Returns the cdxml string representation of this Node."""
         returnString = "<b\n"
         returnString += "id=\"" + id + "\"\n"
-        returnString += "B=\"" + self.begin "\"\n"
-        returnString += "E=\"" + self.end "\"\n"
-        returnString += "Order=\"" + self.order "\"\n"
-        returnString += "Z=\"" + self.zorder "\"\n"
-        returnString += "Display=\"" + self.stereo "\"\n"
+        returnString += "B=\"" + self.begin + "\"\n"
+        returnString += "E=\"" + self.end + "\"\n"
+        returnString += "Order=\"" + self.order + "\"\n"
+        returnString += "Z=\"" + self.zorder + "\"\n"
+        returnString += "Display=\"" + self.stereo + "\"\n"
         returnString += "\/n>"
         return returnString
 
