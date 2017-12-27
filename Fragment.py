dec62e3e4bc0df21970c721f082815b4eddded47
BuchwaldGroup <buchwaldgroup@MavericksImage-2.local>
2017-12-27 00:18:40 -0500

    typo fix

--- Fragment.py
+++ Fragment.py
@@ -1,18 +1,18 @@
 class Fragment(object):
 
-    def __init__(self,id,objectslist):
-        self.objectslist = objectslist
+    def __init__(self,id,objectsList):
+        self.objectsList = objectsList
         self.id = id
 
     def __add__(self,obj):
-        self.idlist.append(obj)
+        self.objectsList.append(obj)
 
     def __str__(self):
         """Returns the cdxml string representation of this Node."""
         returnString = "<fragment\n"
         returnString += "id=\"" + str(id) + "\"\n"
-        for obj in self.objectlist:
-            returnString += obj
+        for obj in self.objectsList:
+            returnString += str(obj)
         returnString += "</fragment>"
 
         return returnString
